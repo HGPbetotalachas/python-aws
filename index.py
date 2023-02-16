8b4ea5cfcf24b2b113ed1a2f5d6524026f791eaa
Eduardo de Jesús <lalo900.99@gmail.com>
2023-02-15 22:40:35 -0600

    cambios en requirements y nombre de la app

--- index.py
+++ index.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def home():
     return render_template ('home.html')
 
-@app.route('/about')
+@application.route('/about')
 def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80, debug=True)
+    application.run(host="0.0.0.0", port=80, debug=True)
